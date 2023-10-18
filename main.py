d574c5280067fadd777932e464f93d1b03c9d3f8
Felixsta123 <felixstavonhagen@gmail.com>
2023-10-18 18:04:12 +0200

    fix: subtitles now sentence by sentence and not speachSegment by speachSegment

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 import xml.etree.ElementTree as ET
 import pandas as pd
 
 # Load and parse the XML file
-# ! hard coded for the POC
 tree = ET.parse('datasets/vocapia/0a40d83a-1353-4f34-9889-c0f591e4d8f7/0ca0d6c6-cd60-43c3-88ad-202ffbac83ee/transcript.xml')
 root = tree.getroot()
 
@@ -16,25 +14,38 @@ subtitles = []
 # Iterate through the SpeechSegment elements
 for speech_segment in segment_list.findall('SpeechSegment'):
     speaker_id = speech_segment.get('spkid')
-    start_time = speech_segment.get('stime')
-    end_time = speech_segment.get('etime')
+    sentence_start_time = speech_segment.get('stime')
     
-    # Concatenate the words to form the transcript text, stripping extra whitespace
-    transcript_text = ' '.join([word.text.strip() for word in speech_segment.findall('Word')])
+    # Initialize an empty sentence and its start time
+    sentence = ''
     
-    # Remove extra spaces around punctuation
-    transcript_text = transcript_text.replace(" .", ".").replace(" ,", ",").replace(" ?", "?").replace(" !", "!").replace("' ", "'")
-    
-    # Create a dictionary with the subtitle data
-    subtitle_data = {
-        'Speaker ID': speaker_id,
-        'Start Time': start_time,
-        'End Time': end_time,
-        'Transcript': transcript_text
-    }
-    
-    # Append the subtitle data to the subtitles list
-    subtitles.append(subtitle_data)
+    # Iterate through the Word elements
+    for word in speech_segment.findall('Word'):
+        word_text = word.text.strip()
+        word_end_time = float(word.get('stime')) + float(word.get('dur'))
+        
+        # Append the word to the current sentence
+        sentence += word_text + ' '
+        
+        # Check for sentence-ending punctuation
+        if word_text.endswith(('.', '!', '?')):
+            # Remove extra spaces around punctuation
+            sentence = sentence.replace(" .", ".").replace(" ,", ",").replace(" ?", "?").replace(" !", "!").replace("' ", "'").strip()
+            
+            # Create a dictionary with the subtitle data
+            subtitle_data = {
+                'Speaker ID': speaker_id,
+                'Start Time': sentence_start_time,
+                'End Time': word_end_time,
+                'Transcript': sentence
+            }
+            
+            # Append the subtitle data to the subtitles list
+            subtitles.append(subtitle_data)
+            
+            # Reset the sentence and update the sentence start time
+            sentence = ''
+            sentence_start_time = word_end_time
 
 # Convert the subtitles list to a DataFrame
 df = pd.DataFrame(subtitles)
@@ -44,8 +55,6 @@ print(df)
 
 # Optionally, save the DataFrame to a CSV file
 df.to_csv('subtitles.csv', index=False)
-
-
 
 # ! other stuff we tried
 
